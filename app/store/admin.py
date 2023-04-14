05efa291d7ae2ca3e198b8a50d04b5e1ce56e150
savvig <breareros@gmail.com>
2023-04-14 17:53:21 +0500

    l3: Filters, Search, Ordering: s1

--- app/store/admin.py
+++ app/store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from store.models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'author_name')
+    list_display_links = ('id', 'name', 'price', 'author_name')
+    search_fields = ('id', 'name', 'price', 'author_name')
+
 # Register your models here.
+# admin.site.register(Book, BookAdmin)
--- app/store/views.py
+++ app/store/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from store.models import Book
 from store.serializers import BookSerializer
@@ -8,3 +10,7 @@ from store.serializers import BookSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['id', 'price']  # Не как в видео! Другая версия Django
+    search_fields = ['name', 'author_name']
+    ordering_fields = ['price', 'author_name']
